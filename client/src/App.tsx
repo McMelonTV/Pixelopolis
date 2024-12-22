8981784535eb3d9bb6044ef73adf921e2e5ebb84
McMelonTV <mcmelontube@gmail.com>
2024-12-22 07:28:02 +0100

    set up phaser

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 // @deno-types="@types/react"
-import { useState, useEffect } from 'react'
-// @ts-expect-error Unable to infer type at the moment
-import reactLogo from './assets/react.svg'
+import { useState, useEffect, useRef } from 'react'
+
+import { IRefPhaserGame, PhaserGame } from './game/PhaserGame.tsx';
+import { MainMenu } from './game/scenes/MainMenu.ts';
+
 import { VITE_CLIENT_ID } from './env.ts'
 
 import { discordSdk } from "./discordSdk.ts";
@@ -82,11 +84,103 @@ function App() {
             });
     }, []);
 
+    // The sprite can only be moved in the MainMenu Scene
+    const [canMoveSprite, setCanMoveSprite] = useState(true);
+
+    //  References to the PhaserGame component (game and scene are exposed)
+    const phaserRef = useRef<IRefPhaserGame | null>(null);
+    const [spritePosition, setSpritePosition] = useState({ x: 0, y: 0 });
+
+    const changeScene = () => {
+
+        if(phaserRef.current)
+        {
+            const scene = phaserRef.current.scene as MainMenu;
+
+            if (scene)
+            {
+                scene.changeScene();
+            }
+        }
+    }
+
+    const moveSprite = () => {
+
+        if(phaserRef.current)
+        {
+
+            const scene = phaserRef.current.scene as MainMenu;
+
+            if (scene && scene.scene.key === 'MainMenu')
+            {
+                // Get the update logo position
+                scene.moveLogo(({ x, y }) => {
+
+                    setSpritePosition({ x, y });
+
+                });
+            }
+        }
+
+    }
+
+    const addSprite = () => {
+
+        if (phaserRef.current)
+        {
+            const scene = phaserRef.current.scene;
+
+            if (scene)
+            {
+                // Add more stars
+                const x = Phaser.Math.Between(64, scene.scale.width - 64);
+                const y = Phaser.Math.Between(64, scene.scale.height - 64);
+
+                //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
+                const star = scene.add.sprite(x, y, 'star');
+
+                //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
+                //  You could, of course, do this from within the Phaser Scene code, but this is just an example
+                //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
+                scene.add.tween({
+                    targets: star,
+                    duration: 500 + Math.random() * 1000,
+                    alpha: 0,
+                    yoyo: true,
+                    repeat: -1
+                });
+            }
+        }
+    }
+
+    // Event emitted from the PhaserGame component
+    const currentScene = (scene: Phaser.Scene) => {
+
+        setCanMoveSprite(scene.scene.key !== 'MainMenu');
+
+    }
+
     return (
         loggedIn ? (
-            <>
-                <div>Channel ID: {discordSdk.channelId}</div>
-            </>
+            <div id="app">
+                <PhaserGame ref={phaserRef} currentActiveScene={currentScene}/>
+                <div>
+                    <p>Channel ID: {discordSdk.channelId}</p>
+                    <div>
+                        <button className="button" onClick={changeScene}>Change Scene</button>
+                    </div>
+                    <div>
+                        <button disabled={canMoveSprite} className="button" onClick={moveSprite}>Toggle Movement
+                        </button>
+                    </div>
+                    <div className="spritePosition">Sprite Position:
+                        <pre>{`{\n  x: ${spritePosition.x}\n  y: ${spritePosition.y}\n}`}</pre>
+                    </div>
+                    <div>
+                        <button className="button" onClick={addSprite}>Add New Sprite</button>
+                    </div>
+                </div>
+            </div>
         ) : (
             <div>Loading...</div>
         )
